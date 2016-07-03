c274db7ba792e06daa6b915e3cc190d77d663aa4
shrumit <shrumit.m@hotmail.com>
2016-07-03 16:50:27 -0400

    Add Node server to serve api and static files

---
+++ server.js
@@ -0,0 +1,32 @@
+var compression = require('compression');
+var express = require('express');
+var app = express();
+var courses = require ('./master.json');
+var search = require('./search.json');
+
+var one_day = 86400000;
+
+app.use('/', function(req,res,next){
+  console.log(req.ip);
+  next();
+});
+
+app.get('/api/:id', function(req, res){
+  console.log(req.params.id);
+  res.end(JSON.stringify(courses[req.params.id]));
+});
+
+app.get('/search/:id', function(req, res){
+  console.log('search');
+  res.end(JSON.stringify(search[req.params.id]));
+})
+
+app.use(compression());
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(3000, function () {
+  console.log("Server started at 3000");
+});
+
+//C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --disable-web-security --user-data-dir=$HOME
