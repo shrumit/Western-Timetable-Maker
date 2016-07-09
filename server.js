1135ed797bd16f6e23ea68761a0c3610498888cd
shrumit <shrumit.m@hotmail.com>
2016-07-09 19:51:27 -0400

    Change score calculation, fix bugs with section selection

--- server.js
+++ server.js
@@ -4,27 +4,25 @@ var app = express();
 var courses = require ('./master.json');
 var search = require('./search.json');
 
-var one_day = 86400000;
+app.use(compression());
 
-app.use('/', function(req,res,next){
+app.use(function(req,res,next){
   console.log(req.ip);
+  //fs.appendFile('iplog.txt', req.ip)
   next();
 });
 
+app.use(express.static('public'));
+
 app.get('/api/:id', function(req, res){
-  console.log(req.params.id);
-  res.end(JSON.stringify(courses[req.params.id]));
+  res.send(JSON.stringify(courses[req.params.id]));
 });
 
 app.get('/search/:id', function(req, res){
-  console.log('search');
-  res.end(JSON.stringify(search[req.params.id]));
+  res.send(JSON.stringify(search[req.params.id]));
 })
 
-app.use(compression());
 
-app.use(express.static(__dirname + '/'));
-
-app.listen(3000, function () {
+app.listen(3000, function(){
   console.log("Server started at 3000");
 });
