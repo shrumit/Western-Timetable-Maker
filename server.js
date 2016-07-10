bf4e9348f7d03e9745eaeba9a6d8a738a139e0a1
shrumit <shrumit.m@hotmail.com>
2016-07-10 11:27:36 -0400

    Ready for production - clean indentation, fix server

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var search = require('./search.json');
 app.use(compression());
 
 app.use(function(req,res,next){
-  console.log(req.ip);
+  //console.log(req.ip);
   //fs.appendFile('iplog.txt', req.ip)
   next();
 });
 
-app.use(express.static('public'));
+app.use(express.static('public/'));
 
 app.get('/api/:id', function(req, res){
   res.send(JSON.stringify(courses[req.params.id]));
@@ -23,6 +23,6 @@ app.get('/search/:id', function(req, res){
 })
 
 
-app.listen(3000, function(){
+app.listen(3000, 'localhost', function(){
   console.log("Server started at 3000");
 });
