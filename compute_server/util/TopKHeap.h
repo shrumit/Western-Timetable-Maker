2ed5dff9c0c252bc8a10257dbced72b42192d1e4
shrumit <shrumit.m@hotmail.com>
2017-06-19 14:19:09 -0400

    Remove redundant TopKHeap template impl

--- compute_server/util/TopKHeap.h
+++ compute_server/util/TopKHeap.h
@@ -1,4 +1,19 @@
 /*
+Copyright (C) Shrumit Mehta 2017
+This file is part of Western Timetable Maker.
+Western Timetable Maker is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+Western Timetable Maker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with Western Timetable Maker.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  *  This data structure stores the topmost K inserted pair entries <int, T>, ranked by the first component (int).
  *
  *  It is implemented using a modified array min-heap and offers O(logK) insertion time.
