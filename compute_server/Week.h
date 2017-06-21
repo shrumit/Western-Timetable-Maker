1ce6a085734e515aec5ba694d3e76630ddef0e53
shrumit <shrumit.m@hotmail.com>
2017-06-20 22:49:28 -0400

    Update format

--- compute_server/Week.h
+++ compute_server/Week.h
@@ -2,14 +2,21 @@
 
 #include <stdint.h>
 
-enum WeekIndex {MON, TUE, WED, THU, FRI, WEEK_SIZE};
+enum WeekIndex { 
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    WEEK_SIZE 
+};
 
-struct Week
-{
-  uint32_t day[5];
-  
-  Week () {
-    for (int i = 0; i < WEEK_SIZE; i++)
-      day[i] = 0;
-  }
+struct Week {
+    uint_fast32_t day[WEEK_SIZE];
+
+    Week()
+    {
+        for (int i = 0; i < WEEK_SIZE; i++)
+            day[i] = 0;
+    }
 };
